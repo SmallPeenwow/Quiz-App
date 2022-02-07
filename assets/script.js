a98343f64532a59ce45f8fe583bcacf0917e4681
SmallPeenwow <bowdencamryn@gmail.com>
2022-02-07 09:54:23 +0200

    Part five

--- assets/script.js
+++ assets/script.js
@@ -43,12 +43,16 @@ const startButton = document.getElementById('startBtn');
 const quizContainer = document.querySelector('.quizInfo'); // Used for questions and quiz start and for all done
 const headerQuestion = document.getElementById('questionInfo');
 const questionAnswersDiv = document.querySelector('#question-answers');
+const enterInitialsDiv = document.querySelector('#enterInitials');
+const doneSubmit = document.querySelector('#endSubmit');
 
 // Keep track of array number
 let arrayNum = 0;
 
-startButton.addEventListener('click', startGame); // start quiz btn
+let pointsScore = 0; // Points total at the end 
 
+startButton.addEventListener('click', startGame); // start quiz btn
+doneSubmit.addEventListner('click', restartGame);
 
 // Starting the game
 function startGame() {
@@ -58,30 +62,37 @@ function startGame() {
 }
 
 function nextQuestion() {
-  questionAnswersDiv.style.display = "none";
   arrayNum++;
-  populateQuiz();
+
+  if(arrayNum === questions.length){
+    questionAnswersDiv.style.display = "none";
+    allDone();
+  } else {
+    populateQuiz();
+  }
 }
 
 // This function is used to compare value of button to the array for correct answer
 function answerSelected() {
+
   let lineBreak = document.createElement('hr');
   let correctOrIncorrect = document.createElement('label'); // creats message to display correct or incorrect
   correctOrIncorrect.setAttribute('id', 'messageText');
 
   if(this.id === questions[arrayNum].answer) { // must still add minus time and points system
-    questionAnswersDiv.appendChild(lineBreak);
+    pointsScore += 5;
     correctOrIncorrect.textContent = 'Correct!';
-    questionAnswersDiv.appendChild(correctOrIncorrect);
   } else {
-    questionAnswersDiv.appendChild(lineBreak);
-    correctOrIncorrect.textContent = 'Incorrect!';
-    questionAnswersDiv.appendChild(correctOrIncorrect);
+    pointsScore < 0 ? pointsScore = 0 : pointsScore -= 1;
+    correctOrIncorrect.textContent = 'Incorrect!';   
   }
+
+  questionAnswersDiv.appendChild(lineBreak);
+  questionAnswersDiv.appendChild(correctOrIncorrect);
 
   setTimeout(function() {
     deleteChild();
-
+    nextQuestion();
   }, 1000);
 
 }
@@ -98,16 +109,43 @@ function populateQuiz() {
     inputElement.setAttribute('value', item);
     inputElement.setAttribute('class', "btnAnswers");
     inputElement.setAttribute('id', item);
-    inputElement.addEventListener('click', answerSelected, false); 
+    inputElement.addEventListener('click', answerSelected); 
     questionAnswersDiv.appendChild(inputElement);
   });
 
   questionAnswersDiv.style.display = "grid";
 }
 
+// Remove event listeners
+// function removeEvent() {
+//   debugger;
+//   for (let i = 0; i > questionAnswersDiv.length; i++){
+//     let answerBtn = querySelector('id').removeEventListener('click', answerSelected)
+//   }
+// }
+
 // Removes the created btns in div question-answers
 function deleteChild() {
   while (questionAnswersDiv.firstChild) {
     questionAnswersDiv.removeChild(questionAnswersDiv.firstChild);
   }
+  headerQuestion.innerText = '';
+}
+
+// This is the function that will show the finally score and enter in initials
+function allDone() {
+  document.querySelector('main').style.width = 'auto';
+  headerQuestion.innerText = 'All done!';
+  let scoreShow = document.querySelector('#score');
+  scoreShow.innerText = `Your final score is ${pointsScore}.`;
+  enterInitialsDiv.style.display = 'grid';
+}
+
+// start from beginning
+function restartGame() {
+  arrayNum = 0;
+  pointsScore = 0;
+  document.querySelector('#score').innerText = '';
+  enterInitialsDiv.style.display = 'none';
+  document.querySelector('main').style.width = '55rem';
 }
